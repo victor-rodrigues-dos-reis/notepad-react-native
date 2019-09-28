7629551c4842924df95ac0c424c11922a62ca7cd
VictorRodriguesDosReis <victor_reis005@hotmail.com>
2019-09-28 10:54:13 -0300

    Adicionado a configuração da Status Bar

---
+++ src/config/StatusBarConfig.js
@@ -0,0 +1,4 @@
+import {StatusBar} from 'react-native';
+
+StatusBar.setBackgroundColor("#6b52ae");    // Muda a cor do background da status bar
+StatusBar.setBarStyle("light-content");     // Muda a cor do conteúdo da status bar
