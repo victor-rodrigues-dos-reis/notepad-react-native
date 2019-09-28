0beeccb6954abf24eec88ed9def0a31abe4136e0
VictorRodriguesDosReis <victor_reis005@hotmail.com>
2019-09-28 10:57:17 -0300

    Adicionado comentários no arquivo routes

--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,16 @@ import {createAppContainer} from 'react-navigation';
 import PageIndex from './pages/index';
 import PageNote from './pages/note';
 
+// Criando as rotas
 const AppNavigator = createStackNavigator({
     Home: PageIndex,
     Note: PageNote
 },{
     defaultNavigationOptions: {
         headerStyle: {
-            backgroundColor: '#4B0CE8',
+            backgroundColor: '#4B0CE8', // Muda a cor de background do navigation header
         },
-        headerTintColor: '#fff'
+        headerTintColor: '#fff' // Muda a cor do texto no navigation header
     }
 });
 
